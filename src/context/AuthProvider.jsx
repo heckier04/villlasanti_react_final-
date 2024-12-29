6bd453f73bb11a8c54f5ed00131032083da26fa1
heckier04 <thiagovillasanti35@gmail.com>
2024-12-29 14:59:31 -0300

    contexto

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,24 @@
+import { createContext, useContext, useState, useEffect } from "react";
+import { auth } from "../services/FireBase";
+import { onAuthStateChanged } from "firebase/auth";
+
+const AuthContext = createContext();
+
+export const useAuth = () => useContext(AuthContext);
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
---
+++ src/context/CartProvider.jsx
@@ -0,0 +1,23 @@
+import { createContext, useContext, useState } from "react";
+
+const CartContext = createContext();
+
+export const useCart = () => useContext(CartContext);
+
+export const CartProvider = ({ children }) => {
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (item) => {
+    setCart((prevCart) => [...prevCart, item]);
+  };
+
+  const removeFromCart = (itemId) => {
+    setCart((prevCart) => prevCart.filter(item => item.id !== itemId));
+  };
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
